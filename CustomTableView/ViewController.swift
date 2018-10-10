4af3796e276f155017c1c1058abb51ed873983ee
Christiawan Eko S <christiawaneko@gmail.com>
2018-10-10 18:06:23 +0700

    Initial Commit

---
+++ CustomTableView/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CustomTableView
+//
+//  Created by Christiawan Eko Saputro on 10/10/18.
+//  Copyright © 2018 Dev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
